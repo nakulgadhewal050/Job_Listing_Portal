95a47971ca8475fd343cd999e904ff8c1174ebaf
nakulgadhewal050 <nakulgadhewal050@gmail.com>
2026-04-06 18:51:19 +0530

    s

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import MyApplication from './pages/MyApplication.jsx';
 import Applications from './pages/Applications.jsx';
 import SavedJobs from './pages/SavedJobs.jsx';
-export const serverUrl = import.meta.env.VITE_SERVER_URL;
+export const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:8080"
 
 
 
